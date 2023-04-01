517a0ca4ed99b1a094cd23de9028846778a5a363
frecodev <freconur@gmail.com>
2023-04-01 17:35:18 -0500

    agregnado cambios de css

--- src/modal/MenuListModal.jsx
+++ src/modal/MenuListModal.jsx
@@ -4,55 +4,85 @@ import { Link } from "react-router-dom";
 import "./menuListModal.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown as down } from "@fortawesome/free-solid-svg-icons";
-const MenuListModal = ({thirdMenuActive, setThirdMenuActive, menuActive, setMenuActive,setSecondMenuActive ,secondMenuActive}) => {
+const MenuListModal = ({ thirdMenuActive, setThirdMenuActive, menuActive, setMenuActive, setSecondMenuActive, secondMenuActive }) => {
   const [activeProducts, setActiveProducts] = useState(false);
 
   return ReactDOM.createPortal(
     <div className="menu-list-modal">
       <ul className={`navbar-list-modal ${menuActive && "navbar-list-modal-active"}`}>
-          <li>
-            <Link onClick={() => setSecondMenuActive(!secondMenuActive)}>
-              productos
-            </Link>
-            <ul className={`second-menu-modal ${secondMenuActive && "secondMenuActive-modal"}`}>
-              <li>
-                <Link onClick={() => setThirdMenuActive(!thirdMenuActive)}>
-                  bts
-                </Link>
-                <ul className={`third-menu-modal ${secondMenuActive && thirdMenuActive && "thirdMenuActive-modal"}`}>
-                  <li>polos</li>
-                  <li>tazas</li>
-                  <li>cojines</li>
-                  <li>cojines mid</li>
-                  <li>medias</li>
-                  <li>libretas</li>
-                  <li>cartucheras</li>
-                  <li>poster</li>
-                </ul>
-              </li>
-              <li>
-                <Link to="/kawaii">
-                  kawaii
-                </Link>
-              </li>
-              <li>
-                <Link to="/alquiler-de-cilindros-biombos">
-                  biombos
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li >
-            <Link to="/nosotros">
-              nosotros
-            </Link>
-          </li>
-          <li>
-            <Link to="contacto">
-              contacto
-            </Link>
-          </li>
-        </ul>
+        <li>
+          <Link onClick={() => setSecondMenuActive(!secondMenuActive)}>
+            productos
+          </Link>
+          <ul className={`second-menu-modal ${secondMenuActive && "secondMenuActive-modal"}`}>
+            <li>
+              <Link onClick={() => setThirdMenuActive(!thirdMenuActive)}>
+                bts
+              </Link>
+              <ul className={`third-menu-modal ${secondMenuActive && thirdMenuActive && "thirdMenuActive-modal"}`}>
+                <li>
+                  
+                </li>
+                <li>
+                <Link to="/bts/tazas">
+                    tazas
+                  </Link>
+                </li>
+                <li>
+                <Link to="/bts/cojines">
+                    cojines
+                  </Link>
+                </li>
+                <li>
+                <Link to="/bts/cojinesMid">
+                    cojinesMid
+                  </Link>
+                </li>
+                <li>
+                <Link to="/bts/medias">
+                    medias
+                  </Link>
+                </li>
+                <li>
+                <Link to="/bts/libretas">
+                    libretas
+                  </Link>
+                </li>
+                <li>
+                <Link to="/bts/cartucheras">
+                    cartucheras
+                  </Link>
+                </li>
+                <li>
+                <Link to="/bts/poster">
+                    poster
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/kawaii">
+                kawaii
+              </Link>
+            </li>
+            <li>
+              <Link to="/alquiler-de-cilindros-biombos">
+                biombos
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li >
+          <Link to="/nosotros">
+            nosotros
+          </Link>
+        </li>
+        <li>
+          <Link to="contacto">
+            contacto
+          </Link>
+        </li>
+      </ul>
     </div>,
     document.getElementById("menuListPortal")
   );
